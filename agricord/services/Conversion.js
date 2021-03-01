99660ac8b56a398839f60928c50c63cc9ef22d06
= <=>
2021-03-01 19:29:57 +0800

    update

---
+++ agricord/services/Conversion.js
@@ -0,0 +1,12 @@
+export default {
+  getLFromMl(value){
+    return parseFloat(parseFloat(value) / 1000); 
+  },
+  getUnitsAbbreviation(units){
+    switch(units){
+      case 'Litres (L)': return 'L';
+      case 'Millilitres (ml)': return 'ml';
+      case 'Kilograms (kg)': return 'kg';
+    }
+  }
+}
