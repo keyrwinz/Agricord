2bbc2bb626a6bd20cf2f06847ca5691d9a064155
LEulin <nylael.nilue18181919@gmail.com>
2021-07-16 17:26:18 +0800

    Update

--- agricord/modules/mixPage/index.js
+++ agricord/modules/mixPage/index.js
@@ -1292,7 +1292,7 @@ const MixPage = (props) => {
           }}
           onSuccess={() => {
             setMixConfirmation(false)
-            props.navigation.navigate('batchStack', {total_volume: parseFloat((appliedRate * partialVal) - (totalRates * partialVal)).toFixed(2), selected_paddock: selectedPaddock, application_rate: appliedRate, appliedArea: appliedArea})
+            props.navigation.navigate('batchStack', {total_volume: parseFloat((appliedRate * partialVal) - (totalRates * partialVal)).toFixed(2), selected_paddock: selectedPaddock, application_rate: appliedRate, appliedArea: partialVal})
             setMixCount(1)
             // props.navigation.navigate('batchStack', {total_volume: parseFloat(appliedRate * partialVal).toFixed(2), selected_paddock: selectedPaddock, application_rate: appliedRate})
           }}
@@ -1310,7 +1310,7 @@ const MixPage = (props) => {
             }}
             onSuccess={() => {
               setMixConfirmation(false)
-              props.navigation.navigate('batchStack', {total_volume: parseFloat((appliedRate * totalArea) - (totalRates * totalArea)).toFixed(2), selected_paddock: selectedPaddock, application_rate: appliedRate})
+              props.navigation.navigate('batchStack', {total_volume: parseFloat((appliedRate * totalArea) - (totalRates * totalArea)).toFixed(2), selected_paddock: selectedPaddock, application_rate: appliedRate, appliedArea: totalArea})
               setMixCount(1)
               // props.navigation.navigate('batchStack', {total_volume: parseFloat(appliedRate * totalArea).toFixed(2), selected_paddock: selectedPaddock, application_rate: appliedRate})
             }}
