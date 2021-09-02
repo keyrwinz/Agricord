b332bd85324e90dd847c54f53f51296ef47ff694
pathran-phantom1823 <patrick.pathran23@gmail.com>
2021-09-02 10:27:21 +0800

    order details, and inventory

--- agricord/modules/inventory/index.js
+++ agricord/modules/inventory/index.js
@@ -162,12 +162,14 @@ const Inventory = (props) => {
         offset: offset,
       }
     }
+    console.log('========', parameter)
     Api.request(route, parameter, response => {
       setLoading(false)
       if (response.data.length > 0) {
-        console.log("[DAAT]", response.data);
-        setData(flag == false ? response.data : _.uniqBy([...data, ...response.data], 'code'))
-        setOffset(flag == false ? 0 : offset + 1);
+        // console.log("[DAAT]", response.data);
+        // let tempData = flag == false ? response.data : _.uniqBy([...data, ...response.data], 'code')
+        setData(response.data)
+        setOffset(flag == false ? 1 : offset + limit);
         // setHerbicideData(response.data)
         // setFungicideData(response.data)
         // setInsecticideData(response.data)
